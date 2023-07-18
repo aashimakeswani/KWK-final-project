378c5fa655172486ecbe7f0eb1c89ab8f9b70be0
Aashima <aashimakeswani@gmail.com>
2023-07-18 09:59:01 -0700

    Initial Commit

---
+++ KWK final project/KWK_final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWK_final_projectApp.swift
+//  KWK final project
+//
+//  Created by scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct KWK_final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
